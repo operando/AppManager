f3c93e942042698dde17418f0faf86cfd996ea3c
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2014-07-21 02:37:25 +0900

    Add activities Package

--- wear/src/main/java/com/operamdo/appmanager/activities/TopActivity.java
+++ wear/src/main/java/com/operamdo/appmanager/activities/TopActivity.java
@@ -1,13 +1,14 @@
-package com.operamdo.appmanager;
+package com.operamdo.appmanager.activities;
 
 import android.app.Activity;
 import android.app.LoaderManager;
 import android.content.Loader;
-import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.operamdo.appmanager.R;
 import com.operamdo.appmanager.adapter.ApplicationListAdapter;
+import com.operando.appmanager.common.ApplicationItem;
 import com.operando.appmanager.common.ApplicationListLoader;
 
 import java.util.List;
@@ -15,7 +16,7 @@ import java.util.List;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class TopActivity extends Activity implements LoaderManager.LoaderCallbacks<List<ApplicationInfo>> {
+public class TopActivity extends Activity implements LoaderManager.LoaderCallbacks<List<ApplicationItem>> {
 
     @InjectView(R.id.app_list)
     ListView mAppList;
@@ -30,7 +31,7 @@ public class TopActivity extends Activity implements LoaderManager.LoaderCallbac
     }
 
     @Override
-    public Loader<List<ApplicationInfo>> onCreateLoader(int id, Bundle args) {
+    public Loader<List<ApplicationItem>> onCreateLoader(int id, Bundle args) {
         ApplicationListLoader appLoader = new ApplicationListLoader(this);
         appLoader.forceLoad();
 
@@ -38,11 +39,11 @@ public class TopActivity extends Activity implements LoaderManager.LoaderCallbac
     }
 
     @Override
-    public void onLoadFinished(Loader<List<ApplicationInfo>> loader, List<ApplicationInfo> data) {
+    public void onLoadFinished(Loader<List<ApplicationItem>> loader, List<ApplicationItem> data) {
         mAppList.setAdapter(new ApplicationListAdapter(this, data));
     }
 
     @Override
-    public void onLoaderReset(Loader<List<ApplicationInfo>> loader) {
+    public void onLoaderReset(Loader<List<ApplicationItem>> loader) {
     }
 }
